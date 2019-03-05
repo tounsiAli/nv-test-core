25a3afe01ba02ce1bd4fa76a2d147d3eb3ece9b1
Amir msai <a.msai@novacapta.de>
2019-03-05 09:18:31 +0100

    Initial commit

--- src/common/models/SPEntities.ts
+++ src/common/models/SPEntities.ts
@@ -16,6 +16,7 @@ export class ItemMenuSetting implements EventData {
   public Label: string; //MenuTitle
   public Url: string; //MenuUrl
   public ProjectType: IProjectType;
+  public IsAdding?:boolean;
 }
 
 export interface IProjectType {
